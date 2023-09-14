d67dd61bc7a9faedbd682b5dc4a8ca23eb8fc10e
mahimahithefish <tasnimmmahi@gmail.com>
2023-09-14 09:31:47 -0500

    server set up with express

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
-const PORT = 6000;
+const PORT = 8000;
 
+app.get("/testServer", (req, res)=>{
+    res.send({
+        data:{
+            studentName: "Tasnim Mahi",
+            major: "Computer Science",
+            grade: "junior"
+        }
+    });
+});
+
+app.get('/', (req, res)=>{
+    res.send("Server working");
+});
 app.listen(PORT, ()=>{
     console.log("Server is running on port " + PORT)
-})
+});
 
