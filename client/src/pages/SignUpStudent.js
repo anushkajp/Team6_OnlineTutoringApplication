5615128d8de5abf1b172b2f89914a3aa413a6da3
anushkajp <ajp190004@utdallas.edu>
2023-09-21 01:22:16 -0500

    Added fields for sign up student, need to add CSS

--- client/src/pages/SignUpStudent.js
+++ client/src/pages/SignUpStudent.js
@@ -1,11 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const SignUpStudent = () => {
-  return (
-    <div>SignUpStudent
-        <p>SignUpStudent</p>
-    </div>
-  )
+
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        interests: [],
+        password: '',
+      });
+
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form data submitted:', formData);
+        // Clear the form fields
+        setFormData({
+          firstName: '',
+          lastName: '',
+          email: '',
+          phone: '', 
+          interests: [],
+          password: '',
+        });
+      };
+    
+      return (
+        <div className="signup-container">
+          <h2>Sign Up</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="firstName">First Name</label>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              /> 
+            </div>
+            <div className="form-group">
+              <label htmlFor="Phone Number">Phone Number</label>
+              <input
+                type="tel"
+                id="phone" 
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <button type="submit">Sign Up</button>
+          </form>
+        </div> 
+      );
 }
 
-export default SignUpStudent
+export default SignUpStudent;
