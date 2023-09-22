374672de34585520fd5b5ae4ced8e60db950e952
diana-le <diana.le.201490@gmail.com>
2023-09-22 14:49:18 -0500

    Created models, basic routes for session, students, and tutors

---
+++ server/src/models/session.js
@@ -0,0 +1,14 @@
+class Session {
+    constructor() {
+        this.tutorId = null;
+        this.studentId = null;
+        this.tutorId = null;
+        this.date = null;
+        this.time = null;
+        this.length = null;
+        this.online = null;
+        this.location = null;
+        this.notes = null;
+        this.review = null;
+    }
+}
---
+++ server/src/models/user.js
@@ -0,0 +1,16 @@
+class User {
+    constructor() {
+        this.firstName = null;
+        this.lastName = null;
+        this.middleName = null;
+        this.password = null;
+        this.userId = null;
+        this.courses = null;
+        this.phone = null;
+        this.email = null;
+        this.major = null;
+        this.longBio = null;
+        this.shortBio = null;
+    }
+}
+module.exports = User
