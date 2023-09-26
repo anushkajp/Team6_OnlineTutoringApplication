65d6b39d35cf89c937031d77b2eb316a9d40a7de
Janmorgen <janmorgensternnls@gmail.com>
2023-09-26 18:58:13 -0500

    Created a test point for firebaseDB

--- server/db.js
+++ server/db.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
     messagingSenderId: "1067384641270",
     appId: "1:1067384641270:web:e11a33425bbc86a43f3ed7",
     measurementId: "G-43L355452Y",
-    databaseURL: "https://tutortopia-4aef4-default-rtdb.firebaseio.com/"
+    databaseURL: "https://tutortopia-7d536-default-rtdb.firebaseio.com/"
 };
 
 const fbApp = initializeApp(firebaseConfig);
@@ -22,7 +22,7 @@ const db = getDatabase(fbApp);
 
 // Gets updates from the database itself everytime its run,
 // TODO: Set once to true to load once and read the value locally.
-function readPath(path, once) {
+ function readPath(path, once) {
     // if (!once) {
     get(child(db, path)).then((snapshot) => {
         if (snapshot.exists()) {
@@ -51,17 +51,19 @@ function readPath(path, once) {
 //     authorPic: picture
 //   };
 // batch writes are possible and could be setup in the future
-function addItem(entity, postData) {
+ function addItem(entity, postData) {
     const newPostKey = push(child(ref(db),entity)).key;
 
     const updates={}
     updates["/"+entity+"/"+newPostKey] = postData;
 
     return update(ref(db),updates);
+    
 }
 
 module.exports = {
-    addItem:addItem(),
-    readPath:readPath()
+    db,
+    addItem,
+    readPath
 }
 
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 // Run the server with npm index.js
 //localhost:8000 (basic lol)
+const {db, addItem,readPath} = require('./db');
 const express = require('express');
 const app = express();
 const PORT = 8000;
@@ -13,6 +14,21 @@ app.get("/testServer", (req, res)=>{
             grade: "junior"
         }
     });
+    
+        const postData={
+            username:"janmorgen",
+            major: "Computer Science",
+            courses: [],
+            phone: "1233211234",
+            email:"jcm191123@utdallas.edu",
+            longBio:"why would i need tutoring if all my grades are Ws",
+            firstName:"Jan",
+            lastName:"Morgenstern",
+            middleName:"Christian",
+            password:"plaintextpasswordtobehashed",
+            rating:5
+        }
+        addItem("test/User",postData);
 });
 
 app.get('/', (req, res)=>{
