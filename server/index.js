02e53b7b7ec025b22114704da01f5ec676790556
mahimahithefish <tasnimmmahi@gmail.com>
2023-09-27 15:52:17 -0500

    added csrf and middleware

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
-// Run the server with npm index.js
-//localhost:8000 (basic lol)
 const express = require('express');
 const app = express();
-const PORT = 8000;
+
+const PORT = process.env.PORT || 8000;
+
+const bodyParser = require("body-parser"); // parse as json objects
+
+// security 
+const csrf = require("csurf");
+const cookieParser = require("cookie-parser");
+const csrfMiddleWare = csrf({cookies: true});
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(csrfMiddleWare);
+
+
+
+
 
 app.get("/testServer", (req, res)=>{
     res.send({
