a787e2a24073f225a177e7d047a8ed7bcd4b3ed3
diana-le <diana.le.201490@gmail.com>
2023-09-27 19:58:42 -0500

    Basic router and model, not connected to db

--- server/src/models/review.js
+++ server/src/models/review.js
@@ -1,6 +1,29 @@
 class Review {
-    constructor () {
-        this.rating = null;
-        this.review = null;
+    constructor (rating, review) {
+        this.rating = rating;
+        this.review = review;
+    }
+    // GET ALL BY TUTOR ID
+    findAll() {
+        return json.Review();
+    }
+    // GET BY ID
+    find(id) {
+        // IF ID IS FOUND, RETURN SUCCESS
+        // IF ID NOT FOUND, RETURN FAILED
+    }
+    // POST
+    create(id) {
+        // IF ID IS FOUND, RETURN SUCCESS
+        // IF ID NOT FOUND, RETURN FAILED
+    }
+    // PATCH
+    update(id, review) {
+        // IF ID IS FOUND, RETURN SUCCESS
+        // IF ID NOT FOUND, RETURN FAILED
+    }
+    delete(id) {
+        // IF ID IS FOUND, RETURN SUCCESS
+        // IF ID NOT FOUND, RETURN FAILED
     }
 }
--- server/src/routes/tutors.js
+++ server/src/routes/tutors.js
@@ -3,33 +3,72 @@ const router = express.Router();
 const Tutor = require('../models/tutor')
 
 // GET ALL
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
     try {
-        const tutor = await Tutor.find();
-    } catch (err) {
-        res.status(500).json({message: err.message})
+        const tutor = Tutor.findAll()
+        res.status(200).json(tutor)
+    }catch (err){
+        res.status(500).json({ message: err.message});
     }
 });
 // GET ONE
-router.get('/:id', (req, res) => {
-    return res.send(
-        'GET HTTP method on tutors/${req.params.id} resource',
-    );
+router.get('/:id', async(req, res) => {
+    try {
+        const tutor = await Tutor.find(req.params.id)
+        res.status(200).json(tutor)
+    }catch (err){
+        res.status(500).json({ message: err.message});
+    }
 });
 // CREATE ONE
-router.post('/', (req, res) => {
-    return res.send('POST HTTP method on new tutors')
+router.post('/', async(req, res) => {
+    const tutor = new Tutor();
+    tutor.firstName = req.body.firstName;
+    tutor.lastName = req.body.lastName;
+    tutor.middleName = req.body.middleName;
+    tutor.password = req.body.passwordl;
+    tutor.userId = req.body.userId;
+    tutor.courses = req.body.courses;
+    tutor.phone = req.body.phone;
+    tutor.email = req.body.email;
+    tutor.major = req.body.major;
+    tutor.longBio = req.body.longBio;
+    tutor.shortBio = req.body.shortBio;
+    try {
+        const newTutor = await Tutor.create(req.params.appointmentId);
+        res.status(201).json(newTutor)
+    }catch (err) {
+        res.status(400).json({ message: err.message});
+    }
 });
 // UPDATE ONE
-router.patch('/:id', (req, res) => {
-    return res.send(
-        'PATCH HTTP method on tutors/${req.params.id} resource',
-    );
+router.patch('/:id', async (req, res) => {
+    const tutor = new Tutor();
+    tutor.firstName = req.body.firstName;
+    tutor.lastName = req.body.lastName;
+    tutor.middleName = req.body.middleName;
+    tutor.password = req.body.password;
+    tutor.userId = req.body.userId;
+    tutor.courses = req.body.courses;
+    tutor.phone = req.body.phone;
+    tutor.email = req.body.email;
+    tutor.major = req.body.major;
+    tutor.longBio = req.body.longBio;
+    tutor.shortBio = req.body.shortBio;
+    try {
+        const newTutor = await tutor.create(req.params.id);
+        res.status(201).json(newTutor)
+    }catch (err) {
+        res.status(400).json({ message: err.message});
+    }
 });
 // DELETE ONE
 router.delete('/:id', (req, res) => {
-    return res.send(
-        'DELETE HTTP method on tutors/${req.params.id} resource',
-    );
+    try {
+        const tutor = Tutor.delete(req.params.id)
+        res.status(200).json(tutor)
+    }catch (err){
+        res.status(500).json({ message: err.message});
+    }
 })
 module.exports = router
