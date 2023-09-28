ef5fbe1dc0d3bf4d9d3a3992f95b38332b10902e
diana-le <diana.le.201490@gmail.com>
2023-09-27 19:58:42 -0500

    Basic router and model, not connected to db

--- server/src/models/student.js
+++ server/src/models/student.js
@@ -1,7 +1,57 @@
-const User = require('./user')
+const User = require('./user');
 
-export default class Student extends User {
-    construtor() {
-        super();
+class Student extends User {
+    constructor(firstName, lastName, middleName,
+        password, userId, courses, phone, email, major,
+        longBio, shortBio, pfp) {
+        super(firstName, lastName, middleName,
+            password, userId, courses, phone, email, major,
+            longBio, shortBio, pfp);
+    }
+    // GET BY ID
+    findAll(id) {
+        this.firstName = "Diana"
+        this.lastName = "Le"
+        this.middleName = null;
+        this.password = "12345"
+        this.userId = "deedee"
+        this.coursees = ['12423', 'data & an']
+        this.phone = '123456789'
+        this.email = 'dianale.201490@gmail.com'
+        this.major = 'cs'
+        this.longBio='hello there'
+        this.shortBio = null
+        this.pfp = null
+        return this
+    }
+    // GET BY ID
+    find(id) {
+        this.firstName = "Diana"
+        this.lastName = "Le"
+        this.middleName = null;
+        this.password = "12345"
+        this.userId = "deedee"
+        this.coursees = ['12423', 'data & an']
+        this.phone = '123456789'
+        this.email = 'dianale.201490@gmail.com'
+        this.major = 'cs'
+        this.longBio='hello there'
+        this.shortBio = null
+        this.pfp = null
+        return this
+    }
+    // POST
+    create(id) {
+        // IF ID IS FOUND, RETURN SUCCESS
+        // IF ID NOT FOUND, RETURN FAILED
+    }
+    // PATCH
+    update(id, review) {
+                // IF ID IS FOUND, RETURN SUCCESS
+        // IF ID NOT FOUND, RETURN FAILED
+    }
+    delete(id) {
+        // IF ID IS FOUND, RETURN SUCCESS
+        // IF ID NOT FOUND, RETURN FAILED
     }
 }
--- server/src/models/tutor.js
+++ server/src/models/tutor.js
@@ -1,9 +1,35 @@
-const User = require('./user')
+const User = require('./user');
+const Availability = require('./availability');
 
 class Tutor extends User {
-    constructor () {
-        super();
-        this.rating = null;
-        this.availability = null;
+    constructor(firstName, lastName, middleName,
+        password, userId, courses, phone, email, major,
+        longBio, shortBio, pfp, rating, week, exceptions) {
+        super(firstName, lastName, middleName,
+            password, userId, courses, phone, email, major,
+            longBio, shortBio, pfp);
+        this.rating = rating;
+        const availability = Availability(week, exceptions);
+        this.availability = availability;
+    }
+    
+    // GET BY ID
+    find(id) {
+        // IF ID IS FOUND, RETURN SUCCESS
+        // IF ID NOT FOUND, RETURN FAILED
+    }
+    // POST
+    create(id) {
+        // IF ID IS FOUND, RETURN SUCCESS
+        // IF ID NOT FOUND, RETURN FAILED
+    }
+    // PATCH
+    update(id, review) {
+                // IF ID IS FOUND, RETURN SUCCESS
+        // IF ID NOT FOUND, RETURN FAILED
+    }
+    delete(id) {
+        // IF ID IS FOUND, RETURN SUCCESS
+        // IF ID NOT FOUND, RETURN FAILED
     }
 }
