fba7324fddbf19caa7a0a1058ca379df51110c38
diana-le <diana.le.201490@gmail.com>
2023-10-02 18:39:41 -0500

    Update export in models, moved functions from models to service class

--- server/src/models/review.js
+++ server/src/models/review.js
@@ -3,27 +3,5 @@ class Review {
         this.rating = rating;
         this.review = review;
     }
-    // GET ALL BY TUTOR ID
-    findAll() {
-        return json.Review();
-    }
-    // GET BY ID
-    find(id) {
-        // IF ID IS FOUND, RETURN SUCCESS
-        // IF ID NOT FOUND, RETURN FAILED
-    }
-    // POST
-    create(id) {
-        // IF ID IS FOUND, RETURN SUCCESS
-        // IF ID NOT FOUND, RETURN FAILED
-    }
-    // PATCH
-    update(id, review) {
-        // IF ID IS FOUND, RETURN SUCCESS
-        // IF ID NOT FOUND, RETURN FAILED
-    }
-    delete(id) {
-        // IF ID IS FOUND, RETURN SUCCESS
-        // IF ID NOT FOUND, RETURN FAILED
-    }
 }
+module.exports = Review
