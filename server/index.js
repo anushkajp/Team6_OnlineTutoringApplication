ade23d291e6c378c530c059ee0ba225c4087b188
mahimahithefish <tasnimmmahi@gmail.com>
2023-10-02 21:50:32 -0500

    modified routing with get functions

--- server/index.js
+++ server/index.js
@@ -10,13 +10,19 @@ const csrf = require("csurf");
 const cookieParser = require("cookie-parser");
 const csrfMiddleWare = csrf({cookies: true});
 
+app.engine("html", require("ejs").renderFile);
+
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(csrfMiddleWare);
 
-
-
-
+// Creating express.js middlewares
+// takes any request and sets a cookie 
+app.all("*", (req, res, next) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    next();
+  });
 
 app.get("/testServer", (req, res)=>{
     res.send({
@@ -32,7 +38,36 @@ app.get("/testServer", (req, res)=>{
 app.get('/', (req, res)=>{
     res.send("Server working");
 });
+
+//routing
+
+const tutorRouter = require('./src/routes/tutors')
+const studentRouter = require('./src/routes/students')
+const sessionRouter = require('./src/routes/sessions')
+const reviewRouter = require('./src/routes/reviews')
+const availabilityRouter = require('./src/routes/availability')
+
+app.get(tutorRouter, (req, res)=>{
+    app.use('/tutor', tutorRouter)
+});
+
+app.get(studentRouter, (req, res)=>{
+    app.use('/student', studentRouter)
+});
+
+app.get(sessionRouter, (req, res)=>{
+    app.use('/session', sessionRouter)
+});
+
+app.get(reviewRouter, (req, res)=>{
+    app.use('/session/review', reviewRouter)
+});
+
+app.get(availabilityRouter, (req, res)=>{
+    app.use('/tutor/availability', availabilityRouter) 
+});
+
 app.listen(PORT, ()=>{
-    console.log("Server is running on port " + PORT)
+    console.log(`Listening on http://localhost:${PORT}`);
 });
 
