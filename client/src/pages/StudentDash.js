b3e28e275862b94bb09607f9df62c1928ce4927e
anushkajp <ajp190004@utdallas.edu>
2023-10-06 13:12:56 -0500

    Changes to Dashboard Tile + Tutor Dashboard page

--- client/src/pages/StudentDash.js
+++ client/src/pages/StudentDash.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Sidebar from '../components/sidebar'
 
 const StudentDash = () => {
   return (
     <div>StudentDash
+        <Sidebar renderType="student"></Sidebar>
         <p>StudentDash</p>
     </div>
   )
--- client/src/pages/TutorDash.js
+++ client/src/pages/TutorDash.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import Sidebar from '../components/sidebar'
+import DashboardTile from '../components/DashboardTile'
 
 const TutorDash = () => {
   return (
-    <div>TutorDash
-        <p>TutorDash</p>
+    <div>
+      <Sidebar renderType="tutor"></Sidebar>
+      <div className="tile_contents">
+          <div className="left_div">
+            <div className="top_div">
+                <div className="container">
+                <div className="left">
+                    <DashboardTile width="300px" height="300px" backgroundColor="white" title="My Tutor Stats">tutor stats here</DashboardTile>
+                </div>  
+                <div className="right">
+                    <div className="top">
+                      <DashboardTile width="140px" height="140px" backgroundColor="#B9CCF3">hours tutored</DashboardTile>
+                    </div>
+                    <div className='bottom'>
+                      <DashboardTile width="140px" height="140px" backgroundColor="#F2B9F3">subjects taught</DashboardTile>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div className="bottom_div">
+              <DashboardTile title="Upcoming Sessions">list view of sessions</DashboardTile>
+            </div>
+          </div>
+          <div className="right_div">
+              <DashboardTile title="Recent Student Reviews">list view of reviews</DashboardTile>
+          </div>
+      </div>
     </div>
   )
 }
