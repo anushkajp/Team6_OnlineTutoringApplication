fc9a07bffddc00ad80c4b3b0e68b301ffb5262fb
anushkajp <ajp190004@utdallas.edu>
2023-10-06 13:12:56 -0500

    Changes to Dashboard Tile + Tutor Dashboard page

---
+++ client/src/components/DashboardTile.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const DashboardTile = (props) => {
+  const style = {
+    width: props.width, 
+    height: props.height,
+    backgroundColor: props.backgroundColor,
+    borderRadius: "30px"
+};
+
+  return (
+    <div style={style}>
+        <h4>{props.title}</h4>
+        {props.children}
+    </div>
+  )
+}
+
+export default DashboardTile;
