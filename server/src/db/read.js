b320099e2d5b364ef01967bbaa93437a8ba588a5
Janmorgen <janmorgensternnls@gmail.com>
2023-10-10 10:19:28 -0500

    Added more read functions

--- server/src/db/read.js
+++ server/src/db/read.js
@@ -3,15 +3,23 @@ module.exports={// Only read user Ids from these tables so references to informa
     getUserIds: async function getUserIds() {
         return Object.keys(await readPath("User"))
     },
+    getUsers: function getUsers(){
+        return readPath("User")
+    },
 
     getTutorIds: async function getTutorIds() {
         return Object.keys(await readPath("Tutor"))
+    },
+    getTutors: function getTutors(){
+        return readPath("Tutor")
     },
 
     getStudentIds: async function getStudentIds() {
         return Object.keys(await readPath("Student"))
     },
-
+    getStudents: function getStudents(){
+        return readPath("Student")
+    },
     // Only read by Id to ensure data is being read with intent
     getUser: function getUser(userId) {
         return readPath("User/" + userId)
@@ -31,13 +39,25 @@ module.exports={// Only read user Ids from these tables so references to informa
     getMajors: function getMajors() {
         return readPath("Major")
     },
+    getMajor: function getMajor(majorId){
+        return readPath("Major/"+majorId)
+    },
     getCourses: function getCourses() {
         return readPath("Course")
+    },
+    getCourse:function getCourse(courseId){
+        return readPath("Course/"+courseId)
     },
     getAppointments: function getAppointments() {
         return readPath("Appointment")
     },
+    getAppointment: function getAppointment(appId){
+        return readPath("Appointment/"+appId)
+    },
     getReviews: function getReviews() {
         return readPath("Review")
+    },
+    getReview: function getReview(reviewId){
+        return readPath("Review/"+reviewId)
     }
 }
