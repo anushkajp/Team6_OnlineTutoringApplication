57d999444085cfad728055a1261c540287b428b1
anushkajp <ajp190004@utdallas.edu>
2023-10-11 00:39:52 -0500

    Student and Tutor Dashboards complete

--- client/src/pages/TutorDash.js
+++ client/src/pages/TutorDash.js
@@ -1,34 +1,48 @@
 import React from 'react'
 import Sidebar from '../components/sidebar'
 import DashboardTile from '../components/DashboardTile'
+import SessionTile from '../components/SessionTile'
 
 const TutorDash = () => {
   return (
-    <div>
-      <Sidebar renderType="tutor"></Sidebar>
+    <div className="dashboardPage">
+      <Sidebar className="dbPageSidebar" renderType="tutor"></Sidebar>
       <div className="tile_contents">
           <div className="left_div">
             <div className="top_div">
                 <div className="container">
                 <div className="left">
-                    <DashboardTile width="300px" height="300px" backgroundColor="white" title="My Tutor Stats">tutor stats here</DashboardTile>
+                    <DashboardTile width="300px" height="300px" backgroundColor="white" title="My Tutor Stats">
+                        
+                    </DashboardTile>
                 </div>  
                 <div className="right">
                     <div className="top">
-                      <DashboardTile width="140px" height="140px" backgroundColor="#B9CCF3">hours tutored</DashboardTile>
+                      <DashboardTile width="140px" height="140px" backgroundColor="#B9CCF3">
+                        <h1>15</h1>
+                        <h6>hours tutored</h6>
+                      </DashboardTile>
                     </div>
                     <div className='bottom'>
-                      <DashboardTile width="140px" height="140px" backgroundColor="#F2B9F3">subjects taught</DashboardTile>
+                      <DashboardTile width="140px" height="140px" backgroundColor="#F2B9F3">
+                        <h1>15</h1>
+                        <h6>subjects learned</h6>
+                      </DashboardTile>
                     </div>
                 </div>
                 </div>
             </div>
             <div className="bottom_div">
-              <DashboardTile title="Upcoming Sessions">list view of sessions</DashboardTile>
+              <DashboardTile cln="sessiontiles" title="Upcoming Sessions">
+                  <SessionTile></SessionTile>
+                  <SessionTile></SessionTile>
+                  <SessionTile></SessionTile>
+                  <SessionTile></SessionTile>
+              </DashboardTile>
             </div>
           </div>
           <div className="right_div">
-              <DashboardTile title="Recent Student Reviews">list view of reviews</DashboardTile>
+              <DashboardTile title="Recent Student Reviews">Loading...</DashboardTile>
           </div>
       </div>
     </div>
