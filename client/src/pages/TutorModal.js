7194d6081baeb7252e09c1c9201de8f9b591b010
Sanjana Jadhav <sxj190054@utdallas.edu>
2023-10-13 12:09:06 -0500

    tutor-avail added

--- client/src/pages/TutorModal.js
+++ client/src/pages/TutorModal.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./TutorModal.css";
 import SearchTutor from "./SearchTutor";
 import { TutorTileCard } from "./TutorTile";
+import pfp1 from "../assets/Profile_Pic_1.png";
 
 export default function TutorModal(props) {
   const { toggle, action } = props;
@@ -9,7 +10,50 @@ export default function TutorModal(props) {
   return (
     <div className={`container-modal ${toggle ? "active" : ""}`}>
       <div className="modal">
-        <div className="close" onClick={action}>hi</div>
+        <div className="img">
+          <img src={pfp1}></img>
+        </div>
+        <div className="txt">Tasnim Mahi</div>
+        <div className="stars">⭐ ⭐ ⭐ ⭐ ⭐ (489)</div>
+        <p className="bio">
+          Experienced tutor passionate about empowering students to excel in
+          math, English language arts, and computer science through personalized
+          guidance and innovative teaching.
+        </p>
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <p className="c-sub">📚 Choose Subject</p>
+        <div className="sub-container">
+          <div className="box">CS 1336</div>
+          <div className="box">CS 2336</div>
+        </div>
+
+        <p className="t-sub">⏰ Choose Time</p>
+        <div className="time-container">
+          <div className="t-box">11 AM</div>
+          <div className="t-box">12 PM</div>
+        </div>
+        <br>
+        </br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <div className="close" onClick={action}>
+          Cancel
+        </div>
+        <div className="book">
+          Book
+        </div>
       </div>
     </div>
   );
