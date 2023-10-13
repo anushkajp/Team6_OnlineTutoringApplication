2f072ce67ddb177a7ff4b9a014437052a9b5c4f8
anushkajp <ajp190004@utdallas.edu>
2023-10-12 23:52:55 -0500

    Completed ListUpcomingSessions. Added option to switch to calendar view

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -17,7 +17,7 @@ const Sidebar = (props) => {
             <ul>
               <li className={isNavItemSelected('/StudentDash') ? 'selected-nav-item' : ''}><a href="/StudentDash">Home</a></li>
               <li className={isNavItemSelected('/SearchTutor') ? 'selected-nav-item' : ''}><a href="/SearchTutor">Search Sessions</a></li>
-              <li className={isNavItemSelected('/Calendar') ? 'selected-nav-item' : ''}><a href="/Calendar">Upcoming Sessions</a></li>
+              <li className={isNavItemSelected('/ListUpcoming') ? 'selected-nav-item' : ''}><a href="/ListUpcoming">Upcoming Sessions</a></li>
               <li className={isNavItemSelected('/SavedCoursesStudent') ? 'selected-nav-item' : ''}><a href="/SavedCoursesStudent">My Saved Courses</a></li>
               <li className={isNavItemSelected('/ProfileSettings') ? 'selected-nav-item' : ''}><a href="/ProfileSettings">Profile Settings</a></li>
             </ul>
@@ -32,7 +32,7 @@ const Sidebar = (props) => {
             <ul>
               <li className={isNavItemSelected('/TutorDash') ? 'selected-nav-item' : ''}><a href="/TutorDash">Tutor Dashboard</a></li>
               <li className={isNavItemSelected('/AddTutorSession') ? 'selected-nav-item' : ''}><a href="/AddTutorSession">Add New Session</a></li>
-              <li className={isNavItemSelected('/Calendar') ? 'selected-nav-item' : ''}><a href="/Calendar">Upcoming Sessions</a></li>
+              <li className={isNavItemSelected('/ListUpcoming') ? 'selected-nav-item' : ''}><a href="/ListUpcoming">Upcoming Sessions</a></li>
               <li className={isNavItemSelected('/TutorReports') ? 'selected-nav-item' : ''}><a href="/TutorReports">My Reports</a></li>
               <li className={isNavItemSelected('/ProfileSettings') ? 'selected-nav-item' : ''}><a href="/ProfileSettings">ProfileSettings</a></li>
             </ul>
--- client/src/pages/ListUpcoming.js
+++ client/src/pages/ListUpcoming.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import Sidebar from '../components/sidebar'
+import SessionTile from '../components/SessionTile'
 
-const ListUpcoming = () => {
+const ListUpcoming = (props) => {
   return (
-    <div>ListUpcoming
-        <p>ListUpcoming</p>
+    <div className="upcomingPage">
+      <div className="sidebar">
+        <Sidebar renderType={props.renderType}></Sidebar>
+      </div>
+      <div className="upcomingSession">
+        <div className="switchView">
+          <nav>
+            <a href="/ListUpcoming">List View | </a>
+            <a href="/Calendar">Calendar View</a>
+          </nav>
+        </div>
+        <div className="upcomingSessionList">
+            <SessionTile></SessionTile>
+            <SessionTile></SessionTile>
+            <SessionTile></SessionTile>
+            <SessionTile></SessionTile>
+            <SessionTile></SessionTile>
+            <SessionTile></SessionTile>
+        </div>
+      </div>
     </div>
   )
 }
