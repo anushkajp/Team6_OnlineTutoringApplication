b74e505342165887aa909c7cfdf1275297ed1ce3
Sanjana Jadhav <sxj190054@utdallas.edu>
2023-10-17 00:23:32 -0500

    setup

--- client/src/pages/TwoFactor.js
+++ client/src/pages/TwoFactor.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import "./TwoFactor.css";
+import logo from "../assets/logo.png";
 
 const TwoFactor = () => {
-  return (
-    <div>TwoFactor
-        <p>TwoFactor</p>
-    </div>
-  )
-}
+  document.body.style.background =
+    "linear-gradient(180deg, #B9CCF3 0%, #F2B9F3 100%)";
 
-export default TwoFactor
+  return (
+    <div className="two-page">
+      <div className="white-box">
+        <p className="two-header">Two-Factor Authentication</p>
+        <p className="two-txt">
+          Please enter the code displayed in your authenticator app.
+        </p>
+        <div className="two-input-container">
+          <div className="two-input">6-digit code</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TwoFactor;
