5f14321fca92a24eaa7417e9d91fe0fe5b4f9b4f
Janmorgen <janmorgensternnls@gmail.com>
2023-10-18 19:22:52 -0500

    Added tests

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 // Run the server with npm index.js
 //localhost:8000 (basic lol)
-const { readPath } = require("./src/db/db");
+const { searchItem } = require("./src/db/db");
 
 const reads = require("./src/db/read")
 const updates = require("./src/db/update")
 const adds = require("./src/db/add")
+const deletes=require("./src/db/delete")
 
 // const {db, addItem,readPath,swaggerDocument,swaggerUi,fbApp} = require('./db');
 const express = require('express');
@@ -21,6 +22,9 @@ app.get("/testInfo", (req, res) => {
         const student = await reads.getStudents()
         const major = await reads.getMajors()
         const course = await reads.getCourses()
+        const review = await reads.getReviews()
+        const search = await searchItem("User","username","bibilivesagain")
+        // console.log(await search())
         res.send({
             // for testing
             data: {
@@ -29,27 +33,78 @@ app.get("/testInfo", (req, res) => {
                 student: student,
                 appointment:appointment,
                 major: major,
-                course: course
+                course: course,
+                review:review,
+                searchs: search
 
             }
+            
 
             // console.log(await readPath("User"))
 
 
         });
+        // console.log(searchItem())
 
     })()
 });
 app.get("/testPost", (req, res) => {
 
     (async () => {
-        // dateAvailable = new Date("2:30 PM")
-        // const tutor = await adds.addTutor("Bibi","Bamble","Duke","saltedhash","bibi4eva","Computer Science",[],"1233211234","bibi4eva@gmail.com","Long Bio","short bio",[dateAvailable],[],null,5,true,5)
-        // const student = await adds.addStudent("Jason","Hemroid","Stevens","Jackintheboxmmm","jroid92","Mechanical Enginneering",[],"1233211234","jroid92@gmail.com","London bridge wouldnt have fallen on my watch. Always hustling")
-        updates.updateUsername("-NgePx3To2rYbOgfYW_g", null)
-        // const major = await adds.addMajor("Computer Science")
-        // majorId = major["id"]
-        // const course = await adds.addCourse(majorId,"Computer Networks","CS4370",3)
+        dateAvailable = new Date().toTimeString()
+        const tutor = await adds.addTutor("Bibi","Bamble","Duke","saltedhash","bibi4eva","Computer Science",[],"1233211234","bibi4eva@gmail.com","Long Bio","short bio",[dateAvailable],[],null,5,true,5)
+        tutorId = await tutor["id"]
+        const student = await adds.addStudent("Jason","Hemroid","Stevens","Jackintheboxmmm","jroid92","Mechanical Enginneering",[],"1233211234","jroid92@gmail.com","London bridge wouldnt have fallen on my watch. Always hustling")
+        studentId =await student["id"]
+        // updates.updateUsername("-NgePx3To2rYbOgfYW_g", null)
+        const major = await adds.addMajor("Computer Science")
+        majorId =await major["id"]
+        const course = await adds.addCourse(majorId,"Computer Networks","CS4370",3)
+        const review = await adds.addReview(tutorId,studentId,0,"")
+        reviewId =await review["id"]
+        const appointment = await adds.addAppointment(tutorId,studentId,dateAvailable,10,true,"www.google.com","",0,reviewId)
+        // for(i in await searchItem("User","username","deedee")){
+        //     deletes.deleteUser(i)
+        // }
+        res.send(
+            "post made"
+        );
+        // res.send({
+        //     // for testing
+        //     data: {
+        //         tutor: tutor,
+        //         student: student,
+        //         major: major,
+        //         course: course
+
+        //     }
+
+            // console.log(await readPath("User"))
+
+
+        // });
+
+    })()
+});
+app.get("/testDelete", (req, res) => {
+
+    (async () => {
+        for (i in await reads.getUsers()){
+            console.log("UserID : " + i)
+            deletes.deleteUser(i)
+        }
+        for(i in await reads.getAppointments()){
+            deletes.deleteAppointment(i)
+        }
+        for(i in await reads.getMajors()){
+            deletes.deleteMajor(i)
+        }
+        for(i in await reads.getCourses()){
+            deletes.deleteCourse(i)
+        }
+        // for(i in await searchItem("User","username","deedee")){
+        //     deletes.deleteUser(i)
+        // }
         res.send(
             "post made"
         );
