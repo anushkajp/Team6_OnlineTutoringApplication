db816defbfa3a100e571009b2e82f16fd551f22b
Janmorgen <janmorgensternnls@gmail.com>
2023-10-20 17:07:14 -0500

    Init

--- client/src/pages/AddTutorSession.js
+++ client/src/pages/AddTutorSession.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
-const AddTutorSession = () => {
+import "./AddTutorSession.css"
+function AddTutorSession() {
   return (
     <div>AddTutorSession
-        <p>AddTutorSession</p>
-    </div>
+      <div class="main-container">
+        <div class="main-columns"></div>
+        <div class="main-columns">
+          <label class="subtitle-text">Enter a timeblock name 
+          <input class="timeblock" value="enter a course name or number (e.g - CS 4485, Computer Architecture)"/>
+            </label>
+            <label class="subtitle-text">Which days would you like to tutor?
+            </label>
+            </div>
+            </div>
+            </div>
   )
 }
 
