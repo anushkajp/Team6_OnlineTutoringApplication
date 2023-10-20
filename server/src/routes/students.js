d7b023d5a4af99c13bb6ace35001728230947520
diana-le <diana.le.201490@gmail.com>
2023-10-20 16:03:49 -0500

    This commit has non working student endpoints

--- server/src/routes/students.js
+++ server/src/routes/students.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const Student = require('../models/student')
 const StudentService = require('../services/studentService')
 // GET ALL
 router.get('/', async (req, res) => {
@@ -34,31 +33,23 @@ router.post('/', bodyParser, async(req, res) => {
 });
 // UPDATE ONE
 router.patch('/:id', bodyParser, async (req, res) => {
-    const student = new Student();
-    student.firstName = req.body.firstName;
-    student.lastName = req.body.lastName;
-    student.middleName = req.body.middleName;
-    student.password = req.body.passwordl;
-    student.userId = req.body.userId;
-    student.courses = req.body.courses;
-    student.phone = req.body.phone;
-    student.email = req.body.email;
-    student.major = req.body.major;
-    student.longBio = req.body.longBio;
-    student.shortBio = req.body.shortBio;
     try {
-        const newStudent = await StudentService.update(req.params.id);
-        res.status(201).json(newStudent)
+        console.log("Student controller patch req.body: " + JSON.stringify(req.body))
+        const newStudent = await StudentService.update(JSON.stringify(id, req.params.id));
+        if (newStudent == false)
+            res.status(304).json({message: "Unable to update student"})
+        else
+            res.status(201).json(newStudent)
     }catch (err) {
         res.status(400).json({ message: err.message});
     }
 });
 // DELETE ONE
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     try {
-        const student = StudentService.delete(req.params.id)
+        const student = await StudentService.delete(req.params.id)
         res.status(200).json(student)
-    }catch (err){
+    }catch (err){   
         res.status(500).json({ message: err.message});
     }
 })
--- server/src/services/studentService.js
+++ server/src/services/studentService.js
@@ -4,14 +4,16 @@ const Student = require("../models/student")
 const { getStudent, getStudents, getUserIds, getUsers, getStudentIds} = require ('../db/read')
 const {updateUsername, updateUserMajor, updateUserPhone, updateUserEmail, updateUserBio,
         updateUserPassword, updateUserProfilePic} = require ('../db/update')
-const {addStudent} = require ('../db/add')
+const {addStudent, addUser} = require ('../db/add')
+const searchItem = require ('../db/db')
+const deleteUser = require ('../db/delete')
 // GET ALL
 class StudentService {
     static async getAll() {
         // IF DB CAN FIND ID RETURN student OBJECT
         try {
-            const students = await getStudents()
-            console.log("StudentService.getStudents() = " + students)
+            const students = JSON.stringify(await getUsers())
+            console.log("StudentService.getAll() = " + students)
             return JSON.parse(students)
         }catch (e) {
             return e
@@ -21,95 +23,105 @@ class StudentService {
     static async getOne(id) {
         const student = new Student()
         try {
-            const studentInfo = await getStudents()
-            console.log("StudentService getOne studentIds: " + studentInfo)
-            let i = 0
-            while (id.localCompare(studentInfo.indexOf(i).userId) != 0) {
-                i++
+            console.log("inside getOne student service \n")
+            const PATH = 'User'
+            const ATTRIBUTE = 'username'
+            const find = await JSON.stringify(searchItem("User", "username", "deedee"))
+            for (i in find){
+                console.log(find[i] + "\n")
             }
-            const student = await getStudent()
-            // for (i in studentIds.length) {
-            //     if studentIds.
-            // }
-            // GET USERIDS
-            // FIND USERID BASED ON USERNMAE
-            // USE GET STUDENT WITH ID
-            student = await getStudent(id)
-            const students = await getUserIds()
-            // students = JSON.stringify(students)
-            constole.log("Students: " + students)
-            // console.log(student)
-            return student
+            console.log("StudentService.getOne(id) find: " + find)
+            console.log(typeof(find))
+            
+            return find
         }catch (e) {
             return e
         }
     }
     // POST
-    static async create(studentData) {
+    static async update(studentData) {
         try {
-            console.log("StudentService.create studentData: " + studentData)
+            console.log("StudentService.update studentData: " + studentData + "\n")
+            const data = JSON.parse(studentData)
+            // FIND USERNAME
+            const id = getUserIds(studentData.userId)
+            if (data.firstName != null)
+                student.firstName = data.firstname
+            if (data.lastName != null)
+                student.lastName = data.lastName;
+            if (data.middleName != null)
+                student.middleName = data.middleName;
+            if (data.password != null)
+                student.password = data.password;
+            if (data.userId != null)
+                student.userId = data.userId;
+            if (data.userName != null)
+                student.userName = data.userId;
+            if (data.courses != null)
+                student.courses = data.courses;
+            if (data.phone != null)
+                student.phone = data.phone;
+            if (data.email != null)
+                student.email = data.email;
+            if (data.major != null)
+                student.major = data.major;
+            if (data.longBio != null)
+                student.longBio = data.longBio;
+            if (data.shortBio != null)
+                student.shortBio = data.shortBio;
+            console.log("StudentService.update student: ", JSON.stringify(student) + "\n")
+            
+            return await getUser(studentInfo.userId)
+        } catch (e) {
+            return e;
+        }
+    }    
+    // PATCH
+    static async create(studentData){ 
+        try {
+            console.log("StudentService.create studentData: " + studentData + "\n")
             const data = JSON.parse(studentData)
             const student = new Student();
-            console.log("StudentData.firstname: ", studentData.firstname)
             student.firstName = data.firstName;
             student.lastName = data.lastName;
             student.middleName = data.middleName;
             student.password = data.password;
             student.userId = data.userId;
-            student.userName = data.userId;
+            student.userName = data.userName;
             student.courses = data.courses;
             student.phone = data.phone;
             student.email = data.email;
             student.major = data.major;
             student.longBio = data.longBio;
             student.shortBio = data.shortBio;
-            console.log("StudentService.create student: ", JSON.stringify(student))
-            const newStudent = await addStudent(
+            // ADD NEW STUDENT TO DB
+            console.log("StudentService student:" + student+ "\n")
+            const studentInfo = JSON.stringify(await addStudent(
                 student.firstName, student.middleName,
-                student.lastName, student.password, student.userId, 
+                student.lastName, student.password, student.userName, 
                 student.major, student.courses, student.phone, 
                 student.email, student.longBio, student.rating, 
                 student.pfp
-            );
-    
-            return newStudent;
-        } catch (e) {
-            return e;
-        }
-    }    
-    // PATCH
-    static async update(student){ 
-        try {
-            if (student.userId != null)
-                await updateUsername(student.id, student.userId)
-            if (student.major != null)
-                await updateUserMajor(student.id, student.major)
-            if (student.password != null)
-                await updateUserPassword(student.id, student.password)
-            if (student.email != null)
-                await updateUserEmail(student.id, student.email)
-            if (student.longBio != null)
-                await updateUserBio(student.id, student.longBio)
-            if (student.phone != null)
-                await updateUserPhone(student.id, student.phone)
-            if (student.pfp != null)
-                await updateUserProfilePic(student.id, student.pfp)
-            return await getTutor(student.userId)
+            ))
+            console.log("StudentService studentInfo: " + studentInfo + "\n")
+            
+            // FIND THE NEW STUDENT FROM DB WITH USERID
+            
+            const PATH = 'User'
+            const ATTRIBUTE = 'username'
+            const newStudent = await searchItem(PATH, ATTRIBUTE, JSON.stringify(student).userName)
+            console.log("StudentService.create newStudent: " + newStudent+ "\n")
+            return newStudent
         }catch (e) {
             return e
         }
     }
     // DELETE
-    static delete(id) {
-        const student = new Student()
+    static async delete(id) {
         try {
-            updateUsername(id, null)
-            updateUserBio(id, null)
-            updateUserEmail(id, null)
-            updateUserPassword(id, null)
-            updateUserMajor(id, null)
-            updateUserProfilePic(id, null)
-            return student
+            const deletedUser = JSON.stringify(await deleteUser(id))
+            console.log("StudentService.delete(" + id + ") = " + deletedUser)
+            return deletedUser
         }catch (e) {
             return e
         }
