4f06e7582196e6f3c8c22ba9495efddac31d4a5a
Sanjana Jadhav <sxj190054@utdallas.edu>
2023-10-20 11:09:02 -0500

    home navbar done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/TT_Logo_Design.png";
+import "./Home.css";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div>Home
-        <p>Home</p>
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const navigateToSignUp = () => {
+    navigate("/SignUpTutor");
+  };
 
-export default Home
+  const navigateToLogin = () => {
+    navigate("/Login");
+  };
+
+  const navigateToContact = () => {
+    navigate("/Contact");
+  };
+
+  const navigateToHome = () => {
+    navigate("/Home");
+  };
+
+  const pageStyle = {
+    background: "#F1EFEF",
+  };
+
+  return (
+    <div style={pageStyle}>
+      <div className="flex-container">
+        <img className="logo-top" src={logo} onClick={navigateToHome} />
+        <div className="link-row">
+          <a href="#">
+            <p onClick={navigateToLogin}>Log in</p>
+          </a>
+          <a href="#">
+            <p onClick={navigateToSignUp}>Sign up</p>
+          </a>
+          <a href="#">
+            <p onClick={navigateToContact}>Contact</p>
+          </a>
+        </div>
+      </div>
+      <div class="flex-row">
+        <div class="flex-container-bottom">Container 1</div>
+        <div class="flex-container-bottom">Container 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
