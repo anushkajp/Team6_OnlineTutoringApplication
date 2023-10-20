d88c7239b996ad1fff50f5609033001854dc61f4
Sanjana Jadhav <sxj190054@utdallas.edu>
2023-10-20 13:48:17 -0500

    name change css

--- client/src/pages/SignUpTutor.js
+++ client/src/pages/SignUpTutor.js
@@ -1,47 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const SignUpTutor = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    subjects: "",
+    profile_photo: "",
+    password: "",
+  });
 
-    const [formData, setFormData] = useState({
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: '',
-        subjects: '',
-        profile_photo: '',
-        password: '',
-      });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-          ...formData,
-          [name]: value,
-        });
-      };
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('Form data submitted:', formData);
-        // Clear the form fields
-        setFormData({
-          firstName: '',
-          lastName: '',
-          email: '',
-          phone: '', 
-          subjects: '',
-          profile_photo: '',
-          password: '',
-        });
-      };
-    
-      return (
-        <div className="page-container">
-          <div className="signup-container">
-          <h2>Start Your Journey Today!</h2>
-          <div className="form-fields">
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form data submitted:", formData);
+    // Clear the form fields
+    setFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      subjects: "",
+      profile_photo: "",
+      password: "",
+    });
+  };
+
+  return (
+    <div className="page-container">
+      <div className="signup-container">
+        <h2>Start Your Journey Today!</h2>
+        <div className="form-fields">
           <form onSubmit={handleSubmit}>
-          <div className="form-group">
+            <div className="form-group">
               <label htmlFor="profile_photo">Upload Profile Photo</label>
               <input
                 type="file"
@@ -83,13 +82,13 @@ const SignUpTutor = () => {
                 value={formData.email}
                 onChange={handleChange}
                 required
-              /> 
+              />
             </div>
             <div className="form-group">
               <label htmlFor="Phone Number">Phone Number</label>
               <input
                 type="tel"
-                id="phone" 
+                id="phone"
                 name="phone"
                 value={formData.phone}
                 onChange={handleChange}
@@ -117,15 +116,17 @@ const SignUpTutor = () => {
                 onChange={handleChange}
               />
             </div>
-            <button className="create_acc_student_button" type="submit">Create Tutor Account</button>
-            </form>
-          </div>
-        </div> 
-        <div className="logo-container">
-          <img src="tutortopialogo.png" alt="logo"/>
+            <button className="create_acc_student_button" type="submit">
+              Create Tutor Account
+            </button>
+          </form>
         </div>
-        </div>
-      );
-}
+      </div>
+      <div className="logo-container-signup">
+        <img src="tutortopialogo.png" alt="logo" />
+      </div>
+    </div>
+  );
+};
 
 export default SignUpTutor;
