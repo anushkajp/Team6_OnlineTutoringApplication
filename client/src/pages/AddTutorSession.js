9c26abb52c57bdb03da89ae2ee50d37afe40d6e5
Janmorgen <janmorgensternnls@gmail.com>
2023-10-20 21:16:04 -0500

    Added styling

--- client/src/pages/AddTutorSession.js
+++ client/src/pages/AddTutorSession.js
@@ -4,49 +4,49 @@ import "./AddTutorSession.css"
 function AddTutorSession() {
 
 
-  const weekDays=[
-    {label:'Su',expanded_label:'Sunday'},
-    {label:'M',expanded_label:'Monday'},
-    {label:'Tu',expanded_label:'Tuesday'},
-    {label:'W',expanded_label:'Wednesday'},
-    {label:'Th',expanded_label:'Thursday'},
-    {label:'Fr',expanded_label:'Friday'},
-    {label:'Sa',expanded_label:'Saturday'}
+  const weekDays = [
+    { label: 'Su', expanded_label: 'Sunday' },
+    { label: 'M', expanded_label: 'Monday' },
+    { label: 'Tu', expanded_label: 'Tuesday' },
+    { label: 'W', expanded_label: 'Wednesday' },
+    { label: 'Th', expanded_label: 'Thursday' },
+    { label: 'Fr', expanded_label: 'Friday' },
+    { label: 'Sa', expanded_label: 'Saturday' }
   ]
 
-  function DayButtons(){
-    const[active,setActive] = useState([])
-    const toggleButton=(index)=>{
-      if(active.includes(index)){
-        setActive(active.filter((activeIndex)=>activeIndex !==index))
-      }else{
-        setActive([...active,index])
+  function DayButtons() {
+    const [active, setActive] = useState([])
+    const toggleButton = (index) => {
+      if (active.includes(index)) {
+        setActive(active.filter((activeIndex) => activeIndex !== index))
+      } else {
+        setActive([...active, index])
       }
     }
-    return(
+    return (
       <div>
-        {weekDays.map((button,index)=>(
+        {weekDays.map((button, index) => (
           <button
-          key={index}
-          className="round-button"
-          style={{ backgroundColor: active.includes(index) ? "#D9D9D9" : "#A92AB7" }}
-          onClick={()=>toggleButton(index)}>{button.label}</button>
+            key={index}
+            className="round-button"
+            style={{ backgroundColor: active.includes(index) ? "#D9D9D9" : "#A92AB7" }}
+            onClick={() => toggleButton(index)}>{button.label}</button>
         ))}
       </div>
     )
   }
-  const types = ['online','in-person']
+  const types = ['online', 'in-person']
 
-  function ModalityButtons(){
-    const[active,setActive] = useState(types[0])
+  function ModalityButtons() {
+    const [active, setActive] = useState(types[0])
 
-    return(
-      <div>
-        {types.map((type)=>(
+    return (
+      <div className = "modality-buttons-container">
+        {types.map((type) => (
           <button
-          key={type}
-          className={active === type? "modality-button" : "modality-button-inactive"}
-          onClick={()=>setActive(type)}>{type}</button>
+            key={type}
+            className={active === type ? "modality-button" : "modality-button-inactive"}
+            onClick={() => setActive(type)}>{type}</button>
         ))}
       </div>
     )
@@ -59,7 +59,7 @@ function AddTutorSession() {
           <div>
             <label class="subtitle-text">Enter a timeblock name </label>
             <div>
-              <input class="timeblock" value="enter a course name or number (e.g - CS 4485, Computer Architecture)" />
+              <input class="timeblock" defaultValue="enter a course name or number (e.g - CS 4485, Computer Architecture)" />
             </div>
 
           </div>
@@ -79,16 +79,23 @@ function AddTutorSession() {
             </div>
           </div>
           <div>
-            <input class="date-input" value="enter a course name or number (e.g - CS 4485, Computer Architecture)" />
+            <input class="date-input" value="10:00 AM (CST)" />
             <label class="subtitle-text">to</label>
-            <input class="date-input" value="enter a course name or number (e.g - CS 4485, Computer Architecture)" />
+            <input class="date-input" value="12:00 AM (CST)" />
             <label class="subtitle-text">for</label>
-            <input class="date-input" value="enter a course name or number (e.g - CS 4485, Computer Architecture)" />
+            <input class="date-input" value="1 Hour blocks" />
 
           </div>
           <div>
             <label class="subtitle-text">What modality do you prefer for your tutoring session?</label>
             {ModalityButtons()}
+          </div>
+          <div>
+            <label className="subtitle-text">Add more details about your tutoring timeblock (optional)</label>
+            <div>
+            <textarea id="outlined-multiline-flexible" className='text-area'></textarea>
+            </div>
+            
           </div>
         </div>
       </div>
