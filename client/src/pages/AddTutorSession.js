e09cb3fb5ff8092816db5ede3e8c7fe7b55bbd2c
Janmorgen <janmorgensternnls@gmail.com>
2023-10-22 17:27:19 -0500

    CSS progress

--- client/src/pages/AddTutorSession.js
+++ client/src/pages/AddTutorSession.js
@@ -4,36 +4,36 @@ import Sidebar from '../components/sidebar'
 import LogoutButton from "../components/LogoutButton"
 function AddTutorSession() {
 
-  const timeBlocks=[
-    {label:"30 Min blocks", data:30},
-    {label:"45 Min blocks", data:45},
-    {label:"1 Hour blocks", data:60},
-    {label:"1 Hour 15 Min blocks", data:90}
+  const timeBlocks = [
+    { label: "30 Min blocks", data: 30 },
+    { label: "45 Min blocks", data: 45 },
+    { label: "1 Hour blocks", data: 60 },
+    { label: "1 Hour 15 Min blocks", data: 90 }
   ]
- function TimeBlockOptions(){
-  const [active, setActive] = useState(timeBlocks[0])
-  function choiceHandler(value){
-    setActive(value)
-    console.log(value)
-  }
-  return(
-    // <div>
-      <select 
-      value={active}
-      onChange={e=> choiceHandler(e.target.value)}
-      className="selection justify-center"
+  function TimeBlockOptions() {
+    const [active, setActive] = useState(timeBlocks[0])
+    function choiceHandler(value) {
+      setActive(value)
+      console.log(value)
+    }
+    return (
+      // <div>
+      <select
+        value={active}
+        onChange={e => choiceHandler(e.target.value)}
+        className="selection justify-center"
       >
-        {timeBlocks.map((option,index)=>(
+        {timeBlocks.map((option, index) => (
           <option
-          key={index}
-          value={option.data}
+            key={index}
+            value={option.data}
           // selected={active===option}
           >{option.label}</option>
         ))}
       </select>
-    // </div>
-  )
- }
+      // </div>
+    )
+  }
   const weekDays = [
     { label: 'Su', expanded_label: 'Sunday' },
     { label: 'M', expanded_label: 'Monday' },
@@ -87,29 +87,27 @@ function AddTutorSession() {
     )
   }
   return (
-    <div>
-      <div className="main-container">
+    // <div className="main-container">
+      <div className="main-columns">
         <div className="left-column">
-          <div>
-            <Sidebar className="dbPageSidebar" renderType="tutor"/>
-          </div>
+            <Sidebar className="dbPageSidebar" renderType="tutor" />
         </div>
-        <div className="main-columns">
-          <div className = "right-column">
-            <div >
-              <label className="subtitle-text justify-left">Enter a timeblock name </label>
-              <LogoutButton></LogoutButton>
-              <div>
-                <input className="timeblock" defaultValue="enter a course name or number (e.g - CS 4485, Computer Architecture)" />
-              </div>
-
-            </div>
+        <div className="right-column">
+          
+          <div >
+            <label className="subtitle-text justify-left">Enter a timeblock name </label>
+            
             <div>
-              <label className="subtitle-text justify-left">Which days would you like to tutor?
-              </label>
-              {/* <div> */}
-              {DayButtons()}
-              {/* <button name='sunday' onClick={handleClick} className="round-button" style={{ backgroundColor: active ? "#D9D9D9" : "#A92AB7" }} />
+              <input className="timeblock" defaultValue="enter a course name or number (e.g - CS 4485, Computer Architecture)" />
+            </div>
+
+          </div>
+          <div>
+            <label className="subtitle-text justify-left">Which days would you like to tutor?
+            </label>
+            {/* <div> */}
+            {DayButtons()}
+            {/* <button name='sunday' onClick={handleClick} className="round-button" style={{ backgroundColor: active ? "#D9D9D9" : "#A92AB7" }} />
               <button onClick={handleClick} className="round-button" style={{ backgroundColor: active ? "#D9D9D9" : "#A92AB7" }} />
               <button onClick={handleClick} className="round-button" style={{ backgroundColor: active ? "#D9D9D9" : "#A92AB7" }} />
               <button onClick={handleClick} className="round-button" style={{ backgroundColor: active ? "#D9D9D9" : "#A92AB7" }} />
@@ -117,33 +115,33 @@ function AddTutorSession() {
               <button onClick={handleClick} className="round-button" style={{ backgroundColor: active ? "#D9D9D9" : "#A92AB7" }} />
               <button onClick={handleClick} className="round-button" style={{ backgroundColor: active ? "#D9D9D9" : "#A92AB7" }} /> */}
 
-              {/* </div> */}
-            </div>
-            <div className="justify-left">
-              <input className="date-input" defaultValue="10:00 AM (CST)" />
-              <label className="subtitle-text">to</label>
-              <input className="date-input" defaultValue="12:00 AM (CST)" />
-              <label className="subtitle-text">for</label>
-              {TimeBlockOptions()}
-              {/* <input className="date-input" value="1 Hour blocks" /> */}
-
-            </div>
-            {/* <div className="container"> */}
-            <label className="subtitle-text justify-left">What modality do you prefer for your tutoring session?</label>
-
             {/* </div> */}
-            {ModalityButtons()}
-            <div >
-              <label className="subtitle-text justify-left">Add more details about your tutoring timeblock (optional)</label>
-              <div>
-                <textarea id="outlined-multiline-flexible" className='text-area justify-left'></textarea>
-              </div>
+          </div>
+          <div className="justify-left">
+            <input className="date-input" defaultValue="10:00 AM (CST)" />
+            <label className="subtitle-text">to</label>
+            <input className="date-input" defaultValue="12:00 AM (CST)" />
+            <label className="subtitle-text">for</label>
+            {TimeBlockOptions()}
+            {/* <input className="date-input" value="1 Hour blocks" /> */}
 
+          </div>
+          {/* <div className="container"> */}
+          <label className="subtitle-text justify-left">What modality do you prefer for your tutoring session?</label>
+
+          {/* </div> */}
+          {ModalityButtons()}
+          <div >
+            <label className="subtitle-text justify-left">Add more details about your tutoring timeblock (optional)</label>
+            <div>
+              <textarea id="outlined-multiline-flexible" className='text-area justify-left'></textarea>
             </div>
+
           </div>
         </div>
-      </div>
-    </div>
+       </div>
+      
+    // </div>
   )
 }
 
