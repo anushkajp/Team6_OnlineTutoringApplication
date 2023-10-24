0c60fb56baf0a0972bc6d7e661e3f37d2c006fcf
Sanjana Jadhav <sxj190054@utdallas.edu>
2023-10-24 01:45:27 -0500

    completed home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,6 +10,10 @@ const Home = () => {
     navigate("/SignUpTutor");
   };
 
+  const navigateToSignUpStudent = () => {
+    navigate("/SignUpStudent");
+  };
+
   const navigateToLogin = () => {
     navigate("/Login");
   };
@@ -35,20 +39,42 @@ const Home = () => {
             <p onClick={navigateToLogin}>Log in</p>
           </a>
           <a href="#">
-            <p onClick={navigateToSignUp}>Sign up</p>
-          </a>
-          <a href="#">
             <p onClick={navigateToContact}>Contact</p>
           </a>
         </div>
       </div>
       <div class="flex-row">
         <div class="flex-container-bottom">
-
-          <img className = "img-laptop" src= {laptop}></img>
-        
+          <img className="img-laptop" src={laptop}></img>
         </div>
-        <div class="flex-container-bottom">Container 2</div>
+        <div class="flex-container-bottom">
+          <p class="header-text-home">
+            Private Tutoring, with the click of a button.
+          </p>
+          <p class="header-text-des">
+            TutorTopia instantly connects you with verified tutors in your area.
+            Get tutoring with the click of a button. Tutor at your most
+            convenient times, plan your sessions!
+          </p>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+
+          <div class="button-nav1">
+            <p class="text1" onClick={navigateToSignUp}> Sign up - Tutor</p>
+          </div>
+          <br></br>
+          <br></br>
+          <div class="button-nav2">
+            <p class="text2" onClick={navigateToSignUpStudent}> Sign up - Student</p>
+          </div>
+        </div>
       </div>
     </div>
   );
