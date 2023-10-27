5fb2493fc565f0f27fbe40c47c7fea005aa95308
anushkajp <ajp190004@utdallas.edu>
2023-10-27 01:21:07 -0500

    pushing some changes in

--- client/src/pages/ListUpcoming.js
+++ client/src/pages/ListUpcoming.js
@@ -1,8 +1,36 @@
 import React from 'react'
 import Sidebar from '../components/sidebar'
 import SessionTile from '../components/SessionTile'
+import { fetchFromAPI } from '../services/api'
 
 const ListUpcoming = (props) => {
+  const [renderSessions, setSessions] = useState([])
+
+  useEffect(() => {
+    fetchFromAPI(`${props.renderType}/${props.userName}`) 
+      .then(data => {
+        const dataArray = Object.entries(data).map(([key, value]) => ({
+          key,
+          firstName: value.firstName,
+          lastName: value.lastName,
+          middleName: value.middleName,
+          password: value.password,
+          userId: value.userId,
+          userName: value.userName,
+          courses: value.courses,
+          phone: value.phone,
+          email: value.email,
+          major: value.major,
+          hours: value.hours,
+          longBio: value.longBio
+        }));
+      })
+      .catch(error => {
+        setData([]);
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="upcomingPage">
       <div className="sidebar">
