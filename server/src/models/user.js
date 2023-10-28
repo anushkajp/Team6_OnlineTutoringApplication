0400f662ba6982fcf0a6dfa665a10e9db9e78800
diana-le <diana.le.201490@gmail.com>
2023-10-28 15:29:23 -0500

    Availability and user

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,13 +1,13 @@
 class User {
     constructor(firstName, lastName, middleName,
-        password, userId, userName, courses, phone, email, major,
+        password, userId, username, courses, phone, email, major,
         hours, longBio, shortBio, pfp) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.middleName = middleName;
         this.password = password;
         this.userId = userId;
-        this.userName = userName;
+        this.username = username;
         this.courses = courses;
         this.phone = phone;
         this.email = email;
