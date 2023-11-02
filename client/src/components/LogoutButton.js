23efb031c917f6c25fe414900afb3e10a3951bae
mahimahithefish <tasnimmmahi@gmail.com>
2023-11-01 21:59:53 -0500

    added styling to the logout button

--- client/src/components/LogoutButton.js
+++ client/src/components/LogoutButton.js
@@ -1,16 +1,40 @@
 import React from 'react'
 import { useNavigate, navigate } from 'react-router';
-import './LogoutButton.css';
 const LogoutButton = () => {
     const navigate = useNavigate();
+
+    const buttonStyle = {
+      width: '267.429px',
+      height: '54.857px',
+      borderRadius: '17.143px',
+      border: 'none',
+      background: 'var(--Bold-Purple, #fff)',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+  };
+
+  const textStyles = {
+      color: '#a92ab7',
+      fontFamily: 'Montserrat',
+      fontSize: '22.286px',
+      fontWeight: '500',
+      lineHeight: '54.857px',
+      textAlign: 'center',
+  };
 
     const handleLogout = () => {
       navigate('/Home');
     };
   
+    // return (
+    //   <button className="logout" onClick={handleLogout}>Log Out</button>
+    // );
     return (
-      <button className="logout" onClick={handleLogout}>Log Out</button>
-    );
+      <button style={buttonStyle} onClick={handleLogout} className="logout-button">
+          <span style={textStyles} className="logout-button-text">Log Out</span>
+      </button>
+  );
 }
 
 export default LogoutButton;
