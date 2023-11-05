54a91287490f077f077b85e38ef4cf9151fdd736
Janmorgen <janmorgensternnls@gmail.com>
2023-11-05 14:40:43 -0600

    Oh my god integration hell is here and it will consume all of us

--- client/src/pages/SignUpStudent.js
+++ client/src/pages/SignUpStudent.js
@@ -1,42 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { fetchFromAPI } from '../services/api'
 class User {
   constructor(firstName, lastName, middleName,
-      password, userId, username, courses, phone, email, major,
-      hours, longBio, shortBio, pfp) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.middleName = middleName;
-      this.password = password;
-      this.userId = userId;
-      this.username = username;
-      this.courses = courses;
-      this.phone = phone;
-      this.email = email;
-      this.major = major;
-      this.hours = hours;
-      this.longBio = longBio;
-      this.shortBio = shortBio;
-      this.pfp = pfp;
+    password, userId, username, courses, phone, email, major,
+    hours, longBio, shortBio, pfp) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.middleName = middleName;
+    this.password = password;
+    this.userId = userId;
+    this.username = username;
+
+    this.phone = phone;
+    this.email = email;
+    this.major = major;
+    this.courses = courses;
+    this.hours = hours;
+    this.longBio = longBio;
+    this.shortBio = shortBio;
+    this.pfp = pfp;
   }
 }
 class Student extends User {
   constructor(firstName, lastName, middleName,
-      password, userId, userName, courses, phone, email, major, hours,
-      longBio, shortBio, pfp) {
-      super(firstName, lastName, middleName,
-          password, userId, userName, courses, phone, email, major,
-          longBio, shortBio, pfp);
+    password, userId, userName, courses, phone, email, major, hours,
+    longBio, shortBio, pfp) {
+    super(firstName, lastName, middleName,
+      password, userId, userName, courses, phone, email, major,
+      longBio, shortBio, pfp);
   }
 }
 
-class Major{
-  constructor(majorName,majorId){
-      this.majorName = majorName
-      this.majorId = majorId
+class Major {
+  constructor(majorName, majorId) {
+    this.majorName = majorName
+    this.majorId = majorId
   }
 
 
+}
+
+class Course {
+  constructor(name, number, department, hours, courseId) {
+    this.name = name; // string
+    this.number = number;
+    this.department = department;
+    this.hours = hours
+    this.courseId = courseId
+  }
 }
 
 const SignUpStudent = () => {
@@ -51,23 +62,30 @@ const SignUpStudent = () => {
   // });
 
   const labelData = {
-    firstName:{"label":"First name"},
-    lastName:{"label":"Last name"},
-    middleName:{"label":"Middle name"},
-    password:{"label":"Password"},
-    username:{"label":"Username"},
-    courses:{"label":"Courses"},
-    phone:{"label":"Phone number"},
-    email:{"label":"Email"},
-    major:{"label":"Major"},
-    pfp:{"label":"Profile Picture"}
+    firstName: { "label": "First name" },
+    lastName: { "label": "Last name" },
+    middleName: { "label": "Middle name" },
+    password: { "label": "Password" },
+    username: { "label": "Username" },
+    courses: { "label": "Courses" },
+    phone: { "label": "Phone number" },
+    email: { "label": "Email" },
+    major: { "label": "Major" },
+    pfp: { "label": "Profile Picture" }
 
 
   }
   const initialStudent = new Student();
   const [student, setStudent] = useState(initialStudent);
 
-  const [major, setMajor] = useState([]);
+  const [majors, setMajors] = useState([]);
+  const [courses, setCourses] = useState([]);
+
+  const initialMajor = new Major();
+  const [selectedMajor, setMajor] = useState(initialMajor);
+
+  const [selectedCourses, setSelectedCourses] = useState([])
+
   // const handleChange = (e) => {
   //   const { name, value } = e.target;
   //   setFormData({
@@ -76,79 +94,148 @@ const SignUpStudent = () => {
   //   });
   // };
 
-  const handleChange = (e)=>{
-    const {name, value} = e.target;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
     setStudent({
       ...student,
-      [name]:value
+      [name]: value
     })
     console.log(student)
   }
-  const getMajors = async (e)=>{
-    await fetchFromAPI(`major/`).then(data=>{
-      for (let i in data){
-        const thisMajor = new Major(i.majorName,Object.keys(i)[0])
-        setMajor([...major,thisMajor])
-      }
-    },(err)=>{console.log(err)})
-  }
+  // const getMajors = (e)=>{
 
-  const createFields = (student,setStudent)=>{
-    const fieldsToSkip=['userId','hours',"longBio","shortBio","middleName"]
-    return(
-    <div className="form-group">
-        {Object.keys(student).map((fields,index)=>{
-          if (fieldsToSkip.includes(fields)){
+  //   fetchFromAPI(`major/`).then(data=>{
+  //     for (let i in data){
+  //       console.log(data[i])
+  //       console.log(data["majorName"])
+  //       const thisMajor = new Major(data[i].majorName,Object.keys(data)[0])
+  //       console.log(thisMajor)
+  //       if(!(thisMajor in major)){
+  //         setMajors([...major,thisMajor])
+  //       }
+
+  //     }
+  //   },(err)=>{
+  //     console.log(err)
+  //   })
+  // }
+  useEffect(() => {
+    fetchFromAPI(`major/`).then(data => { 
+      for (let i in data) { 
+        // console.log(data[i])   
+        // console.log(data["majorName"]) 
+        const thisMajor = new Major(data[i].majorName, Object.keys(data)[0])
+        // console.log(thisMajor)
+        // if(!(thisMajor in majors)){
+        setMajors([...majors, thisMajor])
+        // }
+
+      }
+    }, (err) => {
+      console.log(err)
+    })
+
+  }, [])
+  useEffect(() => {
+    console.log('useEffect for courses is running.');
+    console.log('selectedMajor:', selectedMajor);
+    setCourses([])
+    setSelectedCourses([])
+    
+    if (selectedMajor.majorId) {
+      console.log("Past majorId: " + selectedMajor.majorId) 
+      // console.log(selectedMajor.majorId)
+      fetchFromAPI(`course/${selectedMajor.majorId}`).then(data => {
+        // console.log(data)
+        for (let i in data) {
+          // console.log(data[i])
+          // console.log(data["majorName"])
+          // console.log(Object.keys(data)[0])
+          const thisCourse = new Course(data[i].name, data[i].number, data[i].majorId, data[i].hours, Object.keys(data)[0])
+          // console.log(thisMajor)
+          // if(!(thisMajor in majors)){
+          setCourses([...courses, thisCourse])
+          // }
+
+        }
+      }, (err) => {
+        console.log(err)
+      })
+    }
+  }, [selectedMajor])
+
+  // useEffect()
+  const createFields = (student) => {
+    const fieldsToSkip = ['userId', 'hours', "longBio", "shortBio", "middleName"]
+
+
+    return (
+      <div className="form-group">
+        {Object.keys(student).map((fields, index) => {
+          if (fieldsToSkip.includes(fields)) {
             return null;
           }
           return (
-          <div key={index}  className="form-group">
-            <label key={index} htmlFor={fields}>{labelData[fields]["label"]}</label>
-            {(()=>{
-              if (fields==="major"){
-                getMajors()
-                return(
-                  <select>
-                    {Object.keys(major).map((fields,index)=>{
-                      
-                    })}
-                  </select>
+            <div key={index} className="form-group">
+              <label key={index} htmlFor={fields}>{labelData[fields]["label"]}</label>
+              {(() => {
+                if (fields === "major") {
+                  // getMajors() 
+                  return (
+                    <select  onChange={(e) => {
+                      const selectedMajorName = e.target.value;
+                      const selectedMajorObject = majors.find(major => major.majorName === selectedMajorName);
+                      setMajor(selectedMajorObject);
+                    }}>
+                      {Object.keys(majors).map((fields1) => {
+                        // console.log(fields1+index1)
+                        return (<option key={majors[fields1].majorId} value={majors[fields1].majorName}>{majors[fields1].majorName}</option>)
+                      })}
+                    </select>
 
-                )
-               
+                  )
+
+                }
+                else if (fields === "courses") {
+                  return (
+                    <select defaultValue={selectedCourses} multiple>
+                      {Object.keys(courses).map((fields1) => {
+                        console.log(courses[fields1].courseId)
+                        return (<option key={courses[fields1].courseId} value={courses[fields1].name}>{courses[fields1].name}</option>)
+                      })}
+                    </select>
+
+                  )
+                } else {
+                  return (<input
+                    type={fields === "pfp" ? "file" : "text"}
+                    id={fields}
+                    name={fields}
+                    value={student[fields]}
+                    onChange={handleChange}
+                    // onChange={(e)=>setStudent({...student,[fields]:e.target.value})}
+                    required
+                  />)
+                }
+
+
+              })()
               }
-              else if (fields === "courses"){
-
-              }else{
-                return (<input
-                  type={fields==="pfp"? "file" : "text"}
-                  id={fields}
-                  name={fields}
-                  value={student[fields]}
-                  onChange={handleChange}
-                  // onChange={(e)=>setStudent({...student,[fields]:e.target.value})}
-                  required
-                />)
-              }
 
 
-            })()
-            }
-            
-
-          </div>
+            </div>
           )
         }
-        
-        
-        
+
+
+
         )}
 
-</div>
+      </div>
 
     )
   }
-  
+
   // const handleSubmit = (e) => {
   //   e.preventDefault();
   //   console.log("Form data submitted:", formData);
@@ -253,7 +340,7 @@ const SignUpStudent = () => {
                 required
               />
             </div> */}
-            {createFields(student,setStudent)}
+            {createFields(student, setStudent)}
             <button className="create_acc_student_button" type="submit">
               Create Student Account
             </button>
