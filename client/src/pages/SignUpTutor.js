1453bae7b7b9c7f69afcd87dd68896c874726b7a
Janmorgen <janmorgensternnls@gmail.com>
2023-11-07 01:17:57 -0600

    Sign up tutor mostly integrated

--- client/src/pages/SignUpTutor.js
+++ client/src/pages/SignUpTutor.js
@@ -1,38 +1,204 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
+import { fetchFromAPI,uploadToAPI } from '../services/api'
+import Tutor from '../models/tutor'
+import Major from '../models/major'
+import Course from '../models/course'
+
+
+
+
 
 const SignUpTutor = () => {
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    subjects: "",
-    profile_photo: "",
-    password: "",
-  });
+
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+    setStudent({
+      ...student,
+      [name]: value
+    })
+    console.log(student)
+  }
+
+  const handleCourseSelection=(selectedCourseId, isSelected)=>{
+    setSelectedCourses((prevSelectedCourses)=>{
+      if (isSelected){
+        return [...prevSelectedCourses,selectedCourseId]
+      }else{
+        return prevSelectedCourses.filter((id)=> id !== selectedCourseId)
+      }
+    })
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Form data submitted:", formData);
+    console.log("Form data submitted:", student);
+    (async ()=>{
+      const data = await uploadToAPI("tutor/",student)
+      console.log(data)
+    })()
+
     // Clear the form fields
-    setFormData({
-      firstName: "",
-      lastName: "",
-      email: "",
-      phone: "",
-      subjects: "",
-      profile_photo: "",
-      password: "",
-    });
+    // setStudent(new Student());
   };
+
+  const labelData = {
+    firstName: { "label": "First name" },
+    lastName: { "label": "Last name" },
+    middleName: { "label": "Middle name" },
+    password: { "label": "Password" },
+    username: { "label": "Username" },
+    courses: { "label": "Courses" },
+    phone: { "label": "Phone number" },
+    email: { "label": "Email" },
+    major: { "label": "Major" },
+    pfp: { "label": "Profile Picture" }
+
+
+  }
+  const initialStudent = new Tutor();
+  const [student, setStudent] = useState(initialStudent);
+
+  const [majors, setMajors] = useState([]);
+  const [courses, setCourses] = useState([]);
+
+  const initialMajor = new Major();
+  const [selectedMajor, setMajor] = useState(initialMajor);
+
+  const [selectedCourses, setSelectedCourses] = useState([])
+
+  // NOTE: Change student to general object
+  // NOTE: there are new changes here to be applied to sign up student
+  const CreateFields = (student) => {
+    const fieldsToSkip = ['userId', 'hours', "longBio", "shortBio", "middleName"]
+    useEffect(() => {
+      fetchFromAPI(`major/`).then(data => { 
+        const majorArray=[]
+        const majorKeys=[]
+        for (let i in data) { 
+          const majorKey = i
+          const thisMajor = new Major(data[i].majorName, i)
+          majorArray.push(thisMajor)
+  
+        }
+        setMajors(majorArray)
+        // console.log(majors)
+      }, (err) => {
+        console.log(err)
+      })
+  
+    }, [])
+    useEffect(() => {
+      console.log('useEffect for courses is running.');
+      console.log('selectedMajor:', selectedMajor);
+      setCourses([])
+      setSelectedCourses([])
+      const courseArray=[]
+      const courseKeys=[]
+      if (selectedMajor.majorId) {
+        fetchFromAPI(`course/${selectedMajor.majorId}`).then(data => {
+          console.log(data)  
+          for (let i in data) { 
+            if(Object.keys(data)[0] in courseKeys){
+              continue;
+            }
+            const thisCourse = new Course(data[i].courseName, data[i].courseNumber, data[i].majorId, data[i].creditHours)
+            thisCourse["courseId"] = Object.keys(data)[0]
+            courseArray.push(thisCourse)
+          }
+          setCourses(courseArray)
+  
+        }, (err) => {
+          console.log(err)
+        })
+      }
+      // console.log(courses)
+    }, [selectedMajor])
+  
+    return (
+      <div className="form-group">
+        {Object.keys(student).map((fields, index) => {
+          // console.log(labelData)
+          if (labelData[fields]===undefined) {
+            return null;
+          }
+          return (
+            <div key={index} className="form-group">
+              <label key={index} htmlFor={fields}>{labelData[fields]["label"]}</label>
+              {(() => {
+                if (fields === "major") {
+                  return (
+                    <select  onChange={(e) => {
+                      const selectedMajorName = e.target.value;
+                      const selectedMajorObject = majors.find(major => major.majorName === selectedMajorName);
+                      setMajor(selectedMajorObject);
+                      setStudent({
+                        ...student,
+                        [fields]: selectedMajorObject.majorId
+                      })
+                    }}> 
+                      {majors.map( (major) => (
+                        // console.log(fields1+index1) 
+                        <option key={major.majorId} value={major.majorName}>{major.majorName}</option>
+              ))}
+                    </select>
+  
+                  )
+  
+                }
+                else if (fields === "courses") {
+                  return (
+                    <select defaultValue={selectedCourses} multiple 
+                    onChange={(e) => {
+                      const selectedOptions = Array.from(e.target.selectedOptions);
+                      const selectedCourseIds = selectedOptions.map((option) => option.value);
+        
+                      // Iterate through the selected course IDs and handle selection/deselection
+                      selectedCourseIds.forEach((courseId) => {
+                        const isSelected = selectedCourses.includes(courseId);
+        
+                        // Call the handleCourseSelection function
+                        handleCourseSelection(courseId, isSelected);
+                      });
+                      setStudent({...student, [fields]:selectedCourses})
+                    }}
+                  >
+                      {courses.map((course) => (
+                        <option key={course.courseId} value={course.courseName}>{course.courseName}</option>
+              ))}
+                    </select>
+  
+                  )
+                } else {
+                  return (<input
+                    type={fields === "pfp" ? "file" : "text"}
+                    id={fields}
+                    name={fields}
+                    value={student[fields]}
+                    onChange={handleChange}
+                    // onChange={(e)=>setStudent({...student,[fields]:e.target.value})}
+                    required
+                  />)
+                }
+  
+  
+              })()
+              }
+  
+  
+            </div>
+          )
+        }
+  
+  
+  
+        )}
+  
+      </div>
+  
+    )
+  }
+
 
   return (
     <div className="page-container">
@@ -40,7 +206,8 @@ const SignUpTutor = () => {
         <h2>Start Your Journey Today!</h2>
         <div className="form-fields">
           <form onSubmit={handleSubmit}>
-            <div className="form-group">
+            {CreateFields(student)}
+            {/* <div className="form-group">
               <label htmlFor="profile_photo">Upload Profile Photo</label>
               <input
                 type="file"
@@ -115,7 +282,7 @@ const SignUpTutor = () => {
                 value={formData.subjects}
                 onChange={handleChange}
               />
-            </div>
+            </div> */}
             <button className="create_acc_student_button" type="submit">
               Create Tutor Account
             </button>
