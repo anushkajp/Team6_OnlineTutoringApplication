6b88dc8b2fad38f862a44ea52b4249eda0f30e1b
Janmorgen <janmorgensternnls@gmail.com>
2023-11-07 00:43:14 -0600

    Made post function in client API

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -10,3 +10,16 @@ export function fetchFromAPI(route) {
       }
     });
 }
+
+export function uploadToAPI(route, data){
+  console.log(data)
+  data =JSON.stringify(data)
+  return fetch(`http://localhost:8000/${route}`, {method:"POST",headers:{"content-type":"application/json"},body:data})
+  .then(response=>{
+    if (response.status === 201){
+      return response.json();
+    }else{
+      throw new Error('Request failed with status: ' + response.status)
+    }
+  })
+}
