f51aeca340558a6ee2011d426840f5fa68506bb6
diana-le <diana.le.201490@gmail.com>
2023-11-11 12:51:02 -0600

    wip availability

--- server/src/routes/availability.js
+++ server/src/routes/availability.js
@@ -7,9 +7,9 @@ const CustomError = require('../utils/customError')
 // GET ALL AVAILABILITY FOR A TUTOR BY USERNAME
 router.get('/:username', async (req, res) => {
     try {
-        console.log("[ Availability service get all availability")
+        console.log("[ Availability service get all availability ]")
         const availability = await AvailabilityService.allAvail(req.params.username)
-        console.log(availability)
+        // console.log(availability)
         res.status(200).json(availability)
     }catch (err){
         if (err instanceof CustomError)
@@ -33,19 +33,7 @@ router.get('/:username/:dayOfTheWeek', async(req, res) => {
 // CREATE NEW AVAILABILITY BY DAY OF THE WEEK
 router.post('/:username/:dayOfTheWeek', bodyParser, async(req, res) => {
     try {
-        const newavailability = await AvailabilityService.createAvail(req.params.username, req.params.dayOfTheWeek);
-        res.status(201).json(newavailability)
-    }catch (err) {
-        if (err instanceof CustomError)
-            res.status(err.code).json({message: err.message})
-        else
-            res.status(500).json({ message: err.message});
-    }
-});
-// UPDATE DAY OF THE WEEK AVAILABILITY
-router.patch('/:username/:dayOfTheWeek', bodyParser, async (req, res) => {
-    try {
-        const newavailability = await AvailabilityService.updateAvail(req.params.username, req.params.dayOfTheWeek);
+        const newavailability = await AvailabilityService.updateAvail(req.params.username, req.params.dayOfTheWeek, JSON.stringify(req.body));
         res.status(201).json(newavailability)
     }catch (err) {
         if (err instanceof CustomError)
@@ -57,8 +45,8 @@ router.patch('/:username/:dayOfTheWeek', bodyParser, async (req, res) => {
 // DELETE ALL TIME BLOCKS FOR DAY OF THE WEEK
 router.delete('/:username/:dayOfTheWeek', (req, res) => {
     try {
-        const availability = AvailabilityService.deleteAvail(req.params.id)
-        res.status(200).json(availability)
+        AvailabilityService.deleteAvail(req.params.username, req.params.dayOfTheWeek)
+        res.status(200).json("Deleted successfully")
     }catch (err){
         if (err instanceof CustomError)
             res.status(err.code).json({message: err.message})
--- server/src/services/availabilityService.js
+++ server/src/services/availabilityService.js
@@ -7,9 +7,12 @@ const USER = 'User'
 const USERNAME = 'username'
 const CustomError = require ('../utils/customError')
 const Availability = require ('../models/availability')
+const TutorService = require ('./tutorService')
+const Tutor = require("../models/tutor")
 
 class AvailabilityService{
     static async allAvail(username) {
+        console.log("[ AvailabilityService.allAvail ]")
         // CHECK IF THEY ARE A USER
         const search = await searchItem(USER, USERNAME, username)
         // CHECK IF USER IS A TUTOR
@@ -21,25 +24,60 @@ class AvailabilityService{
                 throw new CustomError("User is not a student", 400)
             }
             // RETURN AVAILABILITY OF STUDENT
-            return tutor.availability
-            // return tutor.weeklyAvailability --> currently in db, will change tho
+            // return tutor.availability
+            return tutor.weeklyAvailability //--> currently in db, will change tho
         }
         else
             throw new CustomError("User not found", 400)
        
     }
-    static async oneAvail(id) {
-
+    static async oneAvail(username, dayOfTheWeek) {
+        console.log("[ AvailabilityService.oneAvail ]")
+        const availability = await this.allAvail(username)
+        if (availability.hasOwnProperty(dayOfTheWeek))
+            return availability[dayOfTheWeek]
+        if (Availability.toObj().hasOwnProperty(dayOfTheWeek))
+            throw new CustomError("No Content",204)
+        throw new CustomError(string, 400)
     }
-    static async createAvail() {
-
+    static async updateAvail(username, dayOfTheWeek, availability) {
+        console.log("[ AvailabilityService.createAvail ]")
+        // PARSE AVAILABILITY
+        const data = JSON.parse(availability)
+        // GET OLD AVAILABILITY
+        const oldAvailability = await this.allAvail(username)
+        // GET TUTOR USERNAME
+        const tutorId = await Object.keys(TutorService.getOne(username))[0]
+        // IF dayOfTheWeek IS A DAY, CONTINUE
+        if (Availability.toObj().hasOwnProperty(dayOfTheWeek)) {
+            // REPLACE OLD DAY OF THE WEEK WITH NEW
+            oldAvailability[dayOfTheWeek] = [data]
+            await update.updateTutorWeeklyAvail(tutorId, oldAvailability)
+            // RETURN OBJECT
+            return await this.oneAvail(username, dayOfTheWeek)
+        }
+        // dayOfTheWeek NOT A DAY
+        const string = dayOfTheWeek + " is not a day of the week"
+        throw new CustomError(string, 400)
     }
-    static async updateAvail() {
-
+    static async deleteAvail(username, dayOfTheWeek) {
+        console.log("[ AvailabilityService.deleteAvail ]")
+        // GET OLD AVAILABILITY
+        const oldAvailability = await this.allAvail(username)
+        // GET TUTOR USERNAME
+        const tutorId = await Object.keys(TutorService.getOne(username))[0]
+        // IF dayOfTheWeek IS A DAY, CONTINUE
+        if (oldAvailability.hasOwnProperty(dayOfTheWeek)) {
+            // REPLACE OLD DAY OF THE WEEK WITH NEW
+            delete oldAvailability[dayOfTheWeek]
+            console.log(oldAvailability)
+            await update.updateTutorWeeklyAvail(tutorId, oldAvailability)
+        }
+        if (Availability.toObj().hasOwnProperty(dayOfTheWeek))
+            throw new CustomError("No Content", 204)
+        // dayOfTheWeek NOT A DAY
+        const string = dayOfTheWeek + " is not a day of the week"
+        throw new CustomError(string, 400)
     }
-    static async deleteAvail() {
-
-    }
-
 }
 module.exports = AvailabilityService
