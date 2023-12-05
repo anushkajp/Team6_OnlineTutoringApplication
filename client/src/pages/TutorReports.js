7cad4efda127bbaf2edce3b03f70310618282117
acrugby <acrugby12@gmail.com>
2023-12-05 10:54:22 -0600

    fixed average of reviews

--- client/src/pages/TutorReports.js
+++ client/src/pages/TutorReports.js
@@ -15,12 +15,12 @@ const TutorReports = (props) => {
           key,
           tutorUsername: value.tutorUsername,
           studentUsername: value.studentUsername,
-          rating: value.rating,
+          rating: parseFloat(value.rating), // Parse rating as a float
           description: value.description      
         }
         ));
         console.log(render_data)
-        setReviews(render_data); //??
+        setReviews(render_data); 
       })
       .catch(error => {
         console.error('Error fetching reviews:', error);
@@ -38,86 +38,14 @@ const TutorReports = (props) => {
       />
     ));
 
-  /*
 
-  Came up with this first:
-  const TutorReports = (props) => {
-  const [reviews, setReviews] = useState([]);
-
-  useEffect(() => {
-    fetchFromAPI('tutor-reviews') // Replace 'tutor-reviews' with the actual API endpoint for tutor reviews
-      .then(data => {
-        setReviews(data); // Assuming the API returns an array of reviews
-      })
-      .catch(error => {
-        console.error('Error fetching reviews:', error);
-      });
-  }, []);
-
-  const tiles = reviews.map((review, index) => (
-    <ReviewTile
-      key={index}
-      class_name={review.class_name}
-      student_name={review.student_name}
-      session_time={review.session_time}
-      session_rating={review.session_rating}
-      session_comments={review.session_comments}
-    />
-  ));
-  */
-
-  //Anushka's code
-
-  /*const sampleJSON = [
-    {
-      "class_name": "AP Computer Science A",
-      "student_name": "Anushka P",
-      "session_time": "3:00 PM Tuesday",
-      "session_rating": 4,
-      "session_comments": "TutorTopia rules"
-    },
-    {
-      "class_name": "Physics 101",
-      "student_name": "John D",
-      "session_time": "10:30 AM Wednesday",
-      "session_rating": 3
-    },
-    {
-      "class_name": "Biology Lab",
-      "student_name": "Samantha L",
-      "session_time": "2:15 PM Monday",
-      "session_rating": 4
-    },
-    {
-      "class_name": "Mathematics Advanced",
-      "student_name": "David S",
-      "session_time": "11:00 AM Thursday",
-      "session_rating": 4
-    },
-    {
-      "class_name": "History of Art",
-      "student_name": "Emily B",
-      "session_time": "4:45 PM Friday",
-      "session_rating": 4,
-      "session_comments" : "She aight"
-    }
-  ]*/
-
-    /*const tiles = sampleJSON.map((review, index) => (
-    <ReviewTile key={index} class_name={review.class_name}
-    student_name={review.student_name}
-    session_time={review.session_time}
-    session_rating={review.session_rating}
-    session_comments={review.session_comments}>
-    </ReviewTile>
-  ));*/
-
-
-  // Calculate the sum of session_rating values
-  //const totalRatings = sampleJSON.reduce((sum, session) => sum + session.session_rating, 0);
+  // Calculate the sum of ratings
+  const totalRatings = reviews.reduce((sum, review) => sum + review.rating, 0);
+  console.log('Total Ratings:', totalRatings);
 
   // Calculate the average rating
-  //const averageRating = totalRatings / sampleJSON.length;
+  const averageRating = reviews.length > 0 ? (totalRatings / reviews.length).toFixed(2) : 0;
+  console.log('Average Rating:', averageRating);
 
 
   return (
@@ -127,7 +55,7 @@ const TutorReports = (props) => {
       </div>
       <div className="reviewInfo">
         <div className="averageButton">
-            <h5 className="averageRating">{4} / 5</h5>
+            <h5 className="averageRating">{averageRating} / 5</h5>
             <h6 className="overallRating">Overall rating from {tiles.length} reviews</h6>
         </div>
         <div className="reviewList">
