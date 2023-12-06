56b991ae95fe9a61cbeb18c1ebe5c3d99b11bb75
anushkajp <ajp190004@utdallas.edu>
2023-12-06 01:45:00 -0600

    Revert "Revert "eevert "Merge pull request #125 from anushkajp/integrations_test"""
    This reverts commit 8eedd40a7fe95ed5c48b6fbb16c66d6038443748.

--- server/src/routes/reviews.js
+++ server/src/routes/reviews.js
@@ -2,11 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Review = require('../models/review')
 const ReviewService = require('../services/reviewService')
-const bodyParser = require('body-parser').json();
-const CustomError = require ('../utils/customError')
-//router.use(express.json())
-
-// GET ALL REVIEWS 
+// GET ALL REVIEWS FOR TUTOR
 router.get('/', async (req,res) => {
     try {
         const review = await ReviewService.getAll()
@@ -15,121 +11,44 @@ router.get('/', async (req,res) => {
         res.status(500).json({ message: err.message});
     }
 });
-
-
-// GET ALL REVIEWS BY USERNAME
-router.get('/:id', async(req, res) => {
+// GET ONE
+router.get('/:apointmentId', (req, res) => {
     try {
-        const reviews = await ReviewService.getAllReviewsByUser(req.params.id)
-        if (reviews === null)
-            res.status(400).json({message: req.params.id + ' is not a valid id'})
-        else 
-            res.status(200).json(reviews)
-    }catch (err) {
+        const review = ReviewService.getOne(req.body.appointmentId)
+        res.status(200).json(review)
+    }catch (err){
         res.status(500).json({ message: err.message});
     }
 });
-
-
-// GET ONE
-// router.get('/:id', async(req, res) => {
-//     (async () =>{
-//         try {
-//             const review = await ReviewService.getOne(req.params.id)
-//             console.log("\nReview routes.get(/:id): Session: " + review)
-//             if (review === false)
-//                 res.status(400).json({message: "Review does not exist"})
-//             else 
-//                 res.status(200).json(review)
-//         }catch (err){
-//             res.status(500).json({ message: err.message});
-//         }
-//     })()
-// });
-
-// GET ONE BY STUDENT & TUTOR USERNAME 
-router.get('/:studentUsername/:tutorUsername', async(req, res) => {
-    (async () =>{
-        try {
-            const review = await ReviewService.getOne(req.params.studentUsername, req.params.tutorUsername)
-            if (review === false)
-                res.status(400).json({message: "Review does not exist"})
-            else 
-                res.status(200).json(review)
-        }catch (err){
-            res.status(500).json({ message: err.message});
-        }
-    })()
-});
-
 // CREATE ONE
-router.post('/', bodyParser, async(req, res) => {
+router.post('/:appointmentId', async (req, res) => {
+    const review = new Review(req.body.rating,req.body.review);
     try {
-        const review = await ReviewService.create(JSON.stringify(req.body));
-        res.status(201).json(review)
+        const newReview = await ReviewService.create(req.params.appointmentId);
+        res.status(201).json(newReview)
     }catch (err) {
-        if (err instanceof CustomError)
-            res.status(err.code).json({message: err.message})
-        else
-            res.status(500).json({ message: err.message});
+        res.status(400).json({ message: err.message});
     }
 });
-
 // UPDATE ONE
-router.patch('/:studentUsername/:tutorUsername', bodyParser, async (req, res) => {
+router.patch('/:appointmentId', async(req, res) => {
+    const review = new Review();
+    review.rating = req.body.rating;
+    review.review = req.body.review;
     try {
-        console.log("Review controller patch req.body: " + JSON.stringify(req.body))
-        const updatedReview = await ReviewService.update(req.params.studentUsername, req.params.tutorUsername,JSON.stringify(req.body));
-        res.status(201).json(updatedReview)
+        const newReview = await ReviewService.update(req.params.appointmentId);
+        res.status(201).json(newReview)
     }catch (err) {
-        if (err instanceof CustomError)
-            res.status(err.code).json({message: err.message})
-        else
-            res.status(500).json({ message: err.message});
+        res.status(400).json({ message: err.message});
     }
 });
-
 // DELETE ONE
-// router.delete('/:id', (req, res) => {
-//     try {
-//         const deletedReview = ReviewService.deleteReview(req.params.id)
-//         if (deletedReview === null) {
-//             res.status(404).json({ message: "Review not found" });
-//         } else {
-//             res.status(200).json(deletedReview);
-//         }
-//     } catch (err) {
-//         res.status(500).json({ message: err.message });
-//     }
-// });
-
-router.get('/:studentUsername/:tutorUsername', async(req, res) => {
-    (async () =>{
-        try {
-            const review = await ReviewService.getOne(req.params.studentUsername, req.params.tutorUsername)
-            if (review === false)
-                res.status(400).json({message: "Review does not exist"})
-            else 
-                res.status(200).json(review)
-        }catch (err){
-            res.status(500).json({ message: err.message});
-        }
-    })()
-});
-
-
-router.delete('/:studentUsername/:tutorUsername', async(req, res) => {
-    (async () =>{
-        try {
-            const deletedReview = await ReviewService.deleteReview(req.params.studentUsername, req.params.tutorUsername)
-            if (deletedReview == null)
-                res.status(400).json({message: "Review does not exist"})
-            else 
-                res.status(200).json(deletedReview)
-        }catch (err){
-            res.status(500).json({ message: err.message});
-        }
-    })()
-});
-
+router.delete('/:appointmentId', (req, res) => {
+    try {
+        const review = ReviewService.delete(req.params.appointmentId)
+        res.status(200).json(review)
+    }catch (err){
+        res.status(500).json({ message: err.message});
+    }
+})
 module.exports = router
