1ab6b212b1b774f4a9a4dccd544b3dde43081c40
anushkajp <ajp190004@utdallas.edu>
2023-12-11 22:39:18 -0600

    (e2e-fix): copium'd cors origin :')

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const PORT = 8000;
 const cors = require('cors');
 //app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument))
 const corsOptions = {
-    origin: 'https://team6-online-tutoring-application.vercel.app',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Include OPTIONS method
     optionsSuccessStatus: 204,
 };
